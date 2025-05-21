24476a2163d60ca65437bd7e1c2131bdecd1035a
Kaleabe Getnet <74538080+Kaleabe575@users.noreply.github.com>
2025-05-21 15:54:24 +0300

    EnhanceImageViewer.user.js

--- EnhanceImageViewer.user.js
+++ EnhanceImageViewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Enhance image viewer
 // @namespace    http://tampermonkey.net/
-// @version      1
+// @version      2
 // @description  Replaces default image galleries with Fancybox v5.0.36 on DeviantArt, Reddit, and X (Twitter).
 // @author       Kaleab Getnet
 // @match        https://www.deviantart.com/*
